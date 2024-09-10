611eb85d9c935d3a12b4ca33c41da85f5f642852
Anonjay4 <fagbenleolajide2021@gmail.com>
2024-09-10 08:50:52 +0100

    nodeassignment

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const ejs = require("ejs")
+
+
+app.set("view engine", "ejs")
+app.use(express.urlencoded({extended:true}))
+
+let userarray = []
+let errormssg
+let errormessage
+
+app.get('/',(request, response)=>{
+    // response.send([
+    //     {name:"jide", food:"noodles"},
+    //     {name:"jomiloju", food:"rice"},
+    //     {name:"oyinda", food:"plantain"},
+    //     {name:"victor", food:"yam"},
+    //     {name:"tosin", food:"beans"},
+    //     {name:"adebayo", food:"amala and abula"}
+    // ]);
+    // console.log(__dirname);
+    
+    // response.sendFile(__dirname + '/index.html');
+    response.render('index')
+
+});
+
+app.get('/signup',(request, response)=>{
+    response.render('signup', {errormssg})
+})
+
+app.post('/register',(req,res)=>{
+    console.log(req.body);
+    let existuser = userarray.find((user)=> user.email === req.body.email)
+    if(existuser){
+        errormssg = 'user already exists'
+        res.redirect("/signup")
+    }else{
+        errormssg = ''
+        userarray.push(req.body)
+        console.log(userarray);
+        res.redirect('/login')
+    }
+})
+
+app.get('/login',(request, response)=>{
+    response.render('login', {errormessage})
+})
+
+app.post('/signin',(req,res)=>{
+    let confirmuser = userarray.find((individual)=> individual.email === req.body.email && individual.password === req.body.password)
+    if (confirmuser) {
+        res.redirect('/')
+        errormessage = ''
+    }else{
+        errormessage = 'Invalid email or password'
+        res.redirect('/login')
+    }
+})
+
+
+const port = 9005;
+
+
+app.listen(port,()=> {
+    console.log(`app started on port ${port} `);
+    
+});
